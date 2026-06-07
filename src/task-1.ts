e06a393b8a4de9cb091bbc7ddae387f5ca2d5c9e
Safronov Ivan <safronovaivan@gmail.com>
2026-06-07 20:22:22 +0200

    changed tasks 1,4

--- src/task-1.ts
+++ src/task-1.ts
@@ -1,9 +1,9 @@
 
-const name: { name: string } = { name: "Alice" };
+const name: string = "Alice";
 const age: number = 30;
 const isOnline: boolean = true;
 
-console.log(`Name: ${name.name}`);
+console.log(`Name: ${name}`);
 console.log(`Age: ${age}`);
 console.log(`Online: ${isOnline}`);
 
